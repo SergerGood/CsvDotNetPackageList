9388844667301edb8fcc9c33cd8986a1bcfab1d1
Sergei Khlebnikov <SergerGood@users.noreply.github.com>
2025-01-28 15:28:53 +0300

    Create JsonObject.cs

---
+++ src/Models/JsonObject.cs
@@ -0,0 +1,14 @@
+namespace CsvDotNetPackageList.Models;
+
+public record JsonObject(int Version, string Parameters, List<Project> Projects);
+
+public record Project(string Path, List<Framework> Frameworks);
+
+public record Framework(
+    string framework,
+    List<TopLevelPackage> TopLevelPackages,
+    List<TransitivePackage>? TransitivePackages);
+
+public record TransitivePackage(string Id, string ResolvedVersion);
+
+public record TopLevelPackage(string Id, string RequestedVersion, string ResolvedVersion, string AutoReferenced);
